6d805d8f51c5eb2cdc9c187b68dadd0a24d88f77
malikasaid <127159130+malikasaid@users.noreply.github.com>
2023-03-07 12:05:42 -0500

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -12,12 +12,7 @@ def encode_password(password):
 
 
 # Define the decoder function
-def decode_password(encoded_password):
-    decoded_password = ""
-    for digit in encoded_password:
-        decoded_digit = str((int(digit) - 3) % 10)
-        decoded_password += decoded_digit
-    return decoded_password
+
 
 
 # Define the main function
